de552c6dd1b207b5f8db9199b6f9d19875ee4400
demi <meintomatenfrosch@gmail.com>
2018-06-25 15:50:50 -0400

    lesson 26

--- HelloWorld.py
+++ HelloWorld.py
@@ -27,8 +27,22 @@ age = 24
 
 #basic calculations
 a = 12 #int
-b = 3.5
+b = 3
 print(a + b)
 print(a / b) #returns a float because python automatically gives a decimal answer when dividing
 print(a // b) #// returns integer value when dividing
 print(a % b) #% gives remainder value if the numbers were divided
+
+#strings- sequence data type of characters
+parrot = "Norwegian Blue"
+print(parrot[3])  #treat string like an array, printing one element
+print(parrot[-1])  #starting at end of string
+print(parrot[0:6])  #printing a range
+print(parrot[-4:-2])
+print(parrot[0:6:2])  #extract characters from 0 to 6, taking every second character
+print(parrot[0::3])  #extract characters from 0 onward, taking every third character
+
+#string operators
+print("Hello\n" * 5)
+today = "monday"
+print("day" in today)  #gives true or false
