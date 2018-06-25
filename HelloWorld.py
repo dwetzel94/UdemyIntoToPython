ebae0217e260e5c7c37581d3adc112c30a566b07
demi <meintomatenfrosch@gmail.com>
2018-06-25 15:32:32 -0400

    lesson 25

--- HelloWorld.py
+++ HelloWorld.py
@@ -18,3 +18,17 @@ anotherSplitString = """This string has been
 split over
 several lines"""
 print(anotherSplitString)
+
+_myName_ = "Demi"
+#variable names are case sensitive
+#print() with + only works with multiples of the same variable type
+
+age = 24
+
+#basic calculations
+a = 12 #int
+b = 3.5
+print(a + b)
+print(a / b) #returns a float because python automatically gives a decimal answer when dividing
+print(a // b) #// returns integer value when dividing
+print(a % b) #% gives remainder value if the numbers were divided
