cf6cc331484520a1c71dcf94e3be6379a88403b2
demi <meintomatenfrosch@gmail.com>
2018-06-25 14:54:24 -0400

    initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+#pound sign makes a comment
+#strings can take either single or double parenthesis
+# greeting = "hello"
+# name = input("Please enter your name \n")
+# print(greeting + ' ' + name)
+
+#back slash: \
+splitString = "This string has been \nsplit over \n3 lines"
+print(splitString)
+
+#\n= line break, \t= tab
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s ...he\'s resting"')
+
+anotherSplitString = """This string has been 
+split over
+several lines"""
+print(anotherSplitString)
