3339a63d20184b4ecf2c05706492193eec8b99ee
demi <meintomatenfrosch@gmail.com>
2018-06-25 16:26:15 -0400

    lesson 27

--- HelloWorld.py
+++ HelloWorld.py
@@ -19,11 +19,8 @@ split over
 several lines"""
 print(anotherSplitString)
 
-_myName_ = "Demi"
 #variable names are case sensitive
 #print() with + only works with multiples of the same variable type
-
-age = 24
 
 #basic calculations
 a = 12 #int
@@ -46,3 +43,29 @@ print(parrot[0::3])  #extract characters from 0 onward, taking every third chara
 print("Hello\n" * 5)
 today = "monday"
 print("day" in today)  #gives true or false
+
+#string formatting
+age = 24
+print("My age is " + str(age) + " years")
+
+#replacement fields
+print("My age is {0} years".format(age))
+print("There are {0} days in {1}, {2}, {3} and {4}".format(30,
+    "April", "June", "September", "November"))
+
+print("January: {2}, February: {0}, March: {2}, April: {1}".format(28, 30, 31))
+
+print("My age is %d years" % age)  #% is an old syntax for string formatting from python 2
+print("My age is %d %s, %d %s" % (age, "years", 6, "months"))  #d= integer, s = string
+
+#for loop
+for i in range(1, 12):
+    print("number %2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
+    #number in front if % is for spacing; ** indicates exponents
+
+print("Pi is approximately %12.50f" % (22 / 7))
+
+#python 3 replacement fields with spaccing formatting
+for i in range(1,12):
+    print("Number {0:2} squared is {1:<4} and cubed is {2:<4}".format(i, i ** 2, i ** 3))
+    # {replacement field number : width} <: left justification
