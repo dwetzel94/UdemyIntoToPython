f393a6a028dbf98c747ffaf19f6fa9ea04b87f1e
demi <meintomatenfrosch@gmail.com>
2018-06-25 21:41:31 -0400

    quiz question and string slicing

--- ForLoops.py
+++ ForLoops.py
@@ -5,7 +5,18 @@
 # for i in range(0, len(number)): #len= funtion that calls the length of a string
 #     print(number[i]) #[] indicates position within the string
 
+#small for loop quiz question
+# for i in range(0, 10):
+#     print(i)
+
 number = "9,223,372,854,775,807"
+cleanedNumber = " "
+
 for i in range(0, len(number)):
     if number[i] in "0123456789": #skips commas
-        print(number[i], end= '')
+        cleanedNumber = cleanedNumber + number[i]
+
+newNumber = int(cleanedNumber)
+print("The number is {} ".format(newNumber))
+
+
