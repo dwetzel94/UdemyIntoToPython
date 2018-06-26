4eeb565e0572f019fcd5d18556ecbcd50562427f
demi <meintomatenfrosch@gmail.com>
2018-06-25 20:54:03 -0400

    using 'in' and simple condition exercise

--- IfProgramFlow.py
+++ IfProgramFlow.py
@@ -1,5 +1,19 @@
-age = int(input("How old are you?\n"))
-if not(age<18):
-    print("You can vote")
+# parrot = "Norwegian blue"
+#
+# letter = input("Enter a character: ")
+#
+# if letter in parrot: #'in' looks for the condition within the parameter
+#     print("Give me a {}, nibba".format(letter))
+# else:
+#     print("I don't need that letter")
+
+#simple condition challenge
+x = 7
+y = 7
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
 else:
-    print("please come back in {} years".format(18 - age))
+    print("x is equal to y")
