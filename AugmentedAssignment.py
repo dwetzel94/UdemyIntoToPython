78da9308296a635b7232566ab34cfef58093869b
demi <meintomatenfrosch@gmail.com>
2018-06-27 11:44:21 -0400

    augmented assignment exercise

--- AugmentedAssignment.py
+++ AugmentedAssignment.py
@@ -17,7 +17,7 @@ print(x)
 x*=2
 print(x)
 
-#strings
+# strings
 greeting = "good"
 greeting += "morning"
 print(greeting)
@@ -25,3 +25,23 @@ print(greeting)
 greeting *= 5
 print(greeting)
 
+# AA exercise
+number = 7
+multiplier = 7
+answer = 0
+
+# number += (multiplier-1)*number
+# print(number)
+
+for i in range(0, multiplier):
+    if i != (multiplier - 1):
+        continue
+    else:
+        number += i*number
+        answer = number
+print(answer)
+
+# OR
+for i in range(multiplier):
+    answer += number
+print(answer)
